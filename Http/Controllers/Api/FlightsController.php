1c879b5408d66185dc39453fcf41c04cee31e0ae
Thiên Ân Lacape <thienan.lacape42@gmail.com>
2025-02-09 09:13:59 +0100

    fix

--- Http/Controllers/Api/FlightsController.php
+++ Http/Controllers/Api/FlightsController.php
@@ -313,6 +313,7 @@ class FlightsController extends Controller
 
         foreach ($flights as $flight) {
             $aircraft = [];
+            $flight = $this->flightService->filterSubfleets($request->user(), $flight);
             foreach ($flight->subfleets as $subfleet) {
                 foreach ($subfleet->aircraft as $acf) {
                     $aircraft[] = $acf['id'];
